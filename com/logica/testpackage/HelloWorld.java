334b6f1a97bdf9352b6812917a821b031ee167d0
Lukas Matena <lukas.matena@gmail.com>
2012-07-05 04:57:38 +0200

    Added language support
    Using resource bundle

--- com/logica/testpackage/HelloWorld.java
+++ com/logica/testpackage/HelloWorld.java
@@ -1,9 +1,31 @@
 package com.logica.testpackage;
 
-class HelloWorld
-{  
-        public static void main(String args[])
-        {
-           System.out.println("Hello World!");
-        }
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+class HelloWorld {  
+	public static void main(String args[]) {
+	
+	String language;
+	String country;
+
+	if (args.length != 2) {
+	    language = new String("en");
+	    country = new String("US");
+	} else {
+	    language = new String(args[0]);
+	    country = new String(args[1]);
+	}
+
+	Locale currentLocale;
+	ResourceBundle messages;
+
+	currentLocale = new Locale(language, country);
+
+		
+		ResourceBundle messages = ResourceBundle.getBundle(
+			"com.logica.testpackage.languages.Messages", currentLocale);
+
+		System.out.println(messages.getString("helloWorld"));
+	}
 }
