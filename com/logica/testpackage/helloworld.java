beddf892e2e7d3e8692614690cd6579b7a2ccaf7
Lukas Matena <lukas.matena@gmail.com>
2012-07-05 03:49:34 +0200

    Created helloworld and package folders.

---
+++ com/logica/testpackage/helloworld.java
@@ -0,0 +1,9 @@
+package com.logica.testpackage;
+
+class myfirstjavaprog
+{  
+        public static void main(String args[])
+        {
+           System.out.println("Hello World!");
+        }
+}
